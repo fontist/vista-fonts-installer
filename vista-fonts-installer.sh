249615c0057ef18d078d6e30a269377ab8035442
HassanAkbar <hassanakbar2@gmail.com>
2022-01-31 17:05:07 +0500

    Fixed missing bracket and typo in readme

--- vista-fonts-installer.sh
+++ vista-fonts-installer.sh
@@ -367,7 +367,7 @@ main() {
   rm -rf "$temp_dir" || \
     errx "Unable to remove temp directory ${temp_dir}."
 
-  if [ "${platform}" == "ubuntu" ] && [ ! $(which fc-cache) ; then
+  if [ "${platform}" == "ubuntu" ] && [ ! $(which fc-cache) ]; then
     echo ":: Cleaning the font cache... " >&2
     fc-cache -f "$MS_FONT_PATH" || \
       errx "Unable to clean font cache via 'fc-cache'. Exiting."
